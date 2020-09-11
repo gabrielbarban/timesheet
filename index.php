62bda64751609586d065c156c510492f6fbdefbd
Gabriel Rocha <gabriel.rocha@confitec.com.br>
2020-09-11 16:59:47 -0300

    conserto div margin

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
 <body>
 
 <div style="margin-top: 30px;"><center><h2><i>controle de horas</i></h2><br><img src="images/timesheet.jpg" width="250px" style="border-radius: 35px;"></center></div>
-<div style="margin-top: 30px;height: 200px;margin-left: 500px;text-align: center; float: center; width: 600px; border-width: 2px; border-style: solid;; border-color: black">
+<center><div style="margin-top: 30px;height: 200px;;text-align: center; float: center; width: 600px; border-width: 2px; border-style: solid;; border-color: black">
 <br><br><br>
 <form action="controller/login.php" method="post">
   Username: <input type="text" name="username"><br>
   Senha:    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="senha"><br><br>
   <input type="submit" value="Entrar">
 </form>
-</div>
+</div></center>
 
 </body>
 </html>
