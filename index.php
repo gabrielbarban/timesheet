5ea8b5b20f6e7506c3ccaf69e30a6254ed13b9c0
Gabriel Rocha <gabriel.rocha@confitec.com.br>
2020-09-11 16:57:58 -0300

    logo na index

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 
-<div style="margin-top: 100px;margin-left: 650px"><h2><i>controle de horas</i></h2></div>
-<div style="margin-top: 100px;height: 200px;margin-left: 650px;text-align: center; float: center; width: 600px; border-width: 2px; border-style: solid;; border-color: black">
+<div style="margin-top: 30px;"><center><h2><i>controle de horas</i></h2><br><img src="images/timesheet.jpg" width="250px" style="border-radius: 35px;"></center></div>
+<div style="margin-top: 30px;height: 200px;margin-left: 500px;text-align: center; float: center; width: 600px; border-width: 2px; border-style: solid;; border-color: black">
 <br><br><br>
 <form action="controller/login.php" method="post">
   Username: <input type="text" name="username"><br>
